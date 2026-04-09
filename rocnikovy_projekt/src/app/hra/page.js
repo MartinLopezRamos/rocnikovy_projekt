9afcc7b99173e790d792b5805a6603069369c730
MartinLopezRamos <sluzhor.tablet@gmail.com>
2026-04-09 12:29:09 +0000

    vytvoření bolka

--- rocnikovy_projekt/src/app/hra/page.js
+++ rocnikovy_projekt/src/app/hra/page.js
@@ -1,9 +1,9 @@
 import Image from "next/image"
 export default function Home() {
     return(
-        <div className="h-[100svh] w-[100%]">
+        <div className="h-[100svh] w-[100%] flex justify-center items-center">
             <Image src="/" alt="pozadi" fill className="object-cover"></Image>
-            <div></div>
+            <Image src="/bolek.png" alt="bolek" width={300} height={300} className="absolute h-[200px] w-[200px]"></Image>
         </div>
     )
 }
