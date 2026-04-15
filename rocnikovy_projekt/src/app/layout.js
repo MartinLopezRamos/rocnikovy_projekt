f36b0e2023c39c66750a3e2fe241fe8f7e1ceb89
MartinLopezRamos <sluzhor.tablet@gmail.com>
2026-04-15 10:35:19 +0000

    vytvoření základů mapy a základů navigace

--- rocnikovy_projekt/src/app/layout.js
+++ rocnikovy_projekt/src/app/layout.js
@@ -11,7 +11,8 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}</body>
     </html>
   );
 }
