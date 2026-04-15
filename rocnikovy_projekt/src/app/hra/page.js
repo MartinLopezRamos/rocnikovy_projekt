d186e6b62f91a0e330b968bbfa4fecb3c07174e8
MartinLopezRamos <sluzhor.tablet@gmail.com>
2026-04-15 10:35:19 +0000

    vytvoření základů mapy a základů navigace

--- rocnikovy_projekt/src/app/hra/page.js
+++ rocnikovy_projekt/src/app/hra/page.js
@@ -1,9 +1,17 @@
 import Image from "next/image"
+import Nav from "./nav"
 export default function Home() {
     return(
-        <div className="h-[100svh] w-[100%] flex justify-center items-center">
-            <Image src="/" alt="pozadi" fill className="object-cover"></Image>
-            <Image src="/bolek.png" alt="bolek" width={300} height={300} className="absolute h-[200px] w-[200px]"></Image>
-        </div>
+        <>
+        <Nav/>
+            <div className="h-[100svh] w-[100%] flex justify-center items-center bg-[#071321]">
+            <div className="h-[768px] w-[1408px] rounded-[40px] p-10 border-blue-400 border-3 flex justify-center items-center
+                bg-[url('/mapa.png')] bg-center bg-cover">
+                    {/* <div className="absolute w-40">
+                        <Image src="/bolek_edited.png" alt="bolek" width={300} height={300} className="object-contain"></Image>
+                    </div> */}
+                </div>
+            </div>
+        </>
     )
 }
