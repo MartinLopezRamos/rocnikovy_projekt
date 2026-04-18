c3462fdcf91101d2d4a7c9fff3e5e1c24247b568
MartinLopezRamos <sluzhor.tablet@gmail.com>
2026-04-18 15:42:39 +0200

    kljjkl

--- rocnikovy_projekt/src/app/places/one/page.js
+++ rocnikovy_projekt/src/app/places/one/page.js
@@ -10,7 +10,7 @@ export default function Home() {
                 </Link>
             <div className="h-[100svh] w-[100%] flex justify-center items-center bg-[#071321]">
                 <div className="h-[768px] w-[1408px] rounded-[40px] p-10 border-blue-400 border-3 flex justify-center items-center
-                    bg-[url('/cardio.png')] bg-center bg-cover"></div>
+                    bg-[url('/cardio_lepsi.png')] bg-center bg-cover"></div>
             </div>
         </>
     )
