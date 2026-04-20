4dbd832c26589836e8b618ccab5c64a7bdd857fb
MartinLopezRamos <sluzhor.tablet@gmail.com>
2026-04-20 09:04:50 +0000

    vytvořeni více lokalit

---
+++ rocnikovy_projekt/src/app/places/restroom/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image"
+import Link from "next/link"
+export default function Home() {
+    return(
+        <>
+            <Link href="/" className="fixed z-999 bg-[#E6B30E] w-12 h-12 flex justify-center items-center ml-10 mt-10 rounded-full">
+                    <div className="relative w-8 h-8 mr-1">
+                        <Image src="/sipka.png" alt="arrow" fill className="object-contain brightness-200"></Image>
+                    </div>
+                </Link>
+            <div className="h-[100svh] w-[100%] flex justify-center items-center bg-[#071321]">
+                <div className="h-[768px] w-[1408px] rounded-[40px] p-10 border-blue-400 border-3 flex justify-center items-center
+                    bg-[url('/restroom.png')] bg-center bg-cover"></div>
+            </div>
+        </>
+    )
+}
