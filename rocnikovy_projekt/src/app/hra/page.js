1eca8d4fef6091c6717bbf3d69d6ed3924f77b5f
MartinLopezRamos <sluzhor.tablet@gmail.com>
2026-04-20 09:04:50 +0000

    vytvořeni více lokalit

--- rocnikovy_projekt/src/app/hra/page.js
+++ rocnikovy_projekt/src/app/hra/page.js
@@ -5,7 +5,7 @@ export default function Home() {
         <Nav/>
             <div className="h-[100svh] w-[100%] flex justify-center items-center bg-[#071321]">
             <div className="h-[768px] w-[1408px] rounded-[40px] p-10 border-blue-400 border-3 flex justify-center items-center
-                bg-[url('/mapa.png')] bg-center bg-cover">
+                bg-[url('/mapa_lepsi.png')] bg-center bg-cover">
                 </div>
             </div>
         </>
