38b766594ee509de8b12cc07f0aa88efef977e37
MartinLopezRamos <sluzhor.tablet@gmail.com>
2026-04-23 11:39:57 +0000

    dokonceni zakladni mapy

--- rocnikovy_projekt/src/app/places/restroom/page.js
+++ rocnikovy_projekt/src/app/places/restroom/page.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Home() {
     return(
         <>
-            <Link href="/" className="fixed z-999 bg-[#E6B30E] w-12 h-12 flex justify-center items-center ml-10 mt-10 rounded-full">
+            <Link href="/hra" className="fixed z-999 bg-[#E6B30E] w-12 h-12 flex justify-center items-center ml-10 mt-10 rounded-full">
                     <div className="relative w-8 h-8 mr-1">
                         <Image src="/sipka.png" alt="arrow" fill className="object-contain brightness-200"></Image>
                     </div>
