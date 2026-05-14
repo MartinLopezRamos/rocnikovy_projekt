3e1e3547c9609e989c3a183440b43d11629382ea
MartinLopezRamos <sluzhor.tablet@gmail.com>
2026-05-14 12:15:21 +0000

    afdsfads

--- rocnikovy_projekt/src/app/places/recepce/page.js
+++ rocnikovy_projekt/src/app/places/recepce/page.js
@@ -15,6 +15,11 @@ export default function Home() {
             <div className="w-60 mt-90 ml-90 absolute">
                 <Image src="/bolek_upgrade.png" alt="bolek" width={300} height={300} className="object-contain"></Image>
             </div>
+            <div className="absolute w-200 h-40 rounded-[30px] bg-[#071321] self-center mt-172 border-blue-400 border-3">
+                <h2 className="text-blue-400 ml-4 text-[25px] mt-2">Bolek</h2>
+                <p className="text-blue-400 ml-4 text-[18px] mt-0 w-[700px] h-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <button className="font-bold text-[40px] text-blue-400 cursor-pointer absolute ml-180 ">→</button>
+            </div>
         </>
     )
 }
